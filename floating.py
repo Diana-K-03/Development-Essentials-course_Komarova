bb6d235e0135997bd48121bbb7bb75008b167327
Diana Komarova <st102763@student.spbu.ru>
2025-10-06 11:43:50 +0000

    Add 3 Python data types scripts

---
+++ floating.py
@@ -0,0 +1,8 @@
+alive_temperature = 42.0
+healthy = 36.6
+dead = 31
+average = (alive_temperature + healthy + dead) / 3
+print("Temperature of a person who is still alive:", alive_temperature)
+print("Temperature of a healthy person:", healthy)
+print("Temperature of a person who is already dead:", dead)
+print("Average hospital temperature:", average )
