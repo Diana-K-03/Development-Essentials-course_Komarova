073c822ce5da33618c913c6d0dbde507b5489ab8
Diana Komarova <st102763@student.spbu.ru>
2025-10-09 14:44:42 +0000

    HA9_GH

---
+++ HA9_lists.py
@@ -0,0 +1,8 @@
+#1. LISTS
+print("1.LISTS")
+dishes = ["spaggetti", "meat", "fish"]
+dishes.append("salat")
+dishes.insert(1, "potato")
+print("List:", dishes)
+print("Last element:", dishes[-1])
+print("Slice [2:4]:", dishes[2:4])
---
+++ HA9_tuples.py
@@ -0,0 +1,6 @@
+#2. TUPLES
+print("2. TUPLES")
+my_tuple = (1, "GSOM", 3.14, True)
+print("Tuple:", my_tuple)
+print("Element at index 3:", my_tuple[3])
+print("Length:", len(my_tuple))
